875563a104517d9ff4303d897e4ce4c110e3c915
Alex Carswell <alexanderb.carswell@gmail.com>
2023-02-04 16:11:32 -0800

    Initial UI functionality

--- src/components/Fullarrow.tsx
+++ src/components/Fullarrow.tsx
@@ -7,38 +7,18 @@ import {
 } from "./plasmic/d_d_npc_generator/PlasmicFullarrow";
 import { HTMLElementRefOf } from "@plasmicapp/react-web";
 
-// Your component props start with props for variants and slots you defined
-// in Plasmic, but you can add more here, like event handlers that you can
-// attach to named nodes in your component.
-//
-// If you don't want to expose certain variants or slots as a prop, you can use
-// Omit to hide them:
-//
-// interface FullarrowProps extends Omit<DefaultFullarrowProps, "hideProps1"|"hideProp2"> {
-//   // etc.
-// }
-//
-// You can also stop extending from DefaultFullarrowProps altogether and have
-// total control over the props for your component.
-export interface FullarrowProps extends DefaultFullarrowProps {}
+export interface FullarrowProps extends DefaultFullarrowProps {
+  click: React.MouseEventHandler<HTMLDivElement>;
+}
 
 function Fullarrow_(props: FullarrowProps, ref: HTMLElementRefOf<"div">) {
-  // Use PlasmicFullarrow to render this component as it was
-  // designed in Plasmic, by activating the appropriate variants,
-  // attaching the appropriate event handlers, etc.  You
-  // can also install whatever React hooks you need here to manage state or
-  // fetch data.
-  //
-  // Props you can pass into PlasmicFullarrow are:
-  // 1. Variants you want to activate,
-  // 2. Contents for slots you want to fill,
-  // 3. Overrides for any named node in the component to attach behavior and data,
-  // 4. Props to set on the root node.
-  //
-  // By default, we are just piping all FullarrowProps here, but feel free
-  // to do whatever works for you.
+  const rest = props as DefaultFullarrowProps;
 
-  return <PlasmicFullarrow root={{ ref }} {...props} />;
+  return <PlasmicFullarrow
+    root={{ ref }}
+    onClick={ props.click }
+    {...rest}
+  />;
 }
 
 const Fullarrow = React.forwardRef(Fullarrow_);
--- src/components/Homepage.tsx
+++ src/components/Homepage.tsx
@@ -4,16 +4,137 @@ import {
   DefaultHomepageProps
 } from "./plasmic/d_d_npc_generator/PlasmicHomepage";
 import { HTMLElementRefOf } from "@plasmicapp/react-web";
-import {NPC} from "../models/npc";
+import {ALIGNMENT, NPC} from "../models/npc";
+import { useState } from "react";
+
+const dummyNPC: NPC = {
+  first_name: 'Sir Reginald',
+  last_name: 'Barnaby Rutherford',
+  portrait: undefined,
+  age: 76,
+  gender: true,
+  height_inches: 72,
+  weight_lbs: 160,
+  str: 18,
+  dex: 14,
+  con: 10,
+  int: 12,
+  wis: 11,
+  cha: 17,
+  alignment: ALIGNMENT.LN,
+  class: 'Knight',
+  physical_description: 'Old knight guy',
+  personality_description: 'HUUUUUUUURRRRRAHHHH',
+  history: 'Campaigns, crusades, wars, oh my',
+  plot_hook: 'foobar',
+}
+
+const dummyNPCs: NPC[] = [{
+  first_name: 'Some',
+  last_name: 'Guy',
+  portrait: undefined,
+  age: 21,
+  gender: true,
+  height_inches: 72,
+  weight_lbs: 140,
+  str: 18,
+  dex: 14,
+  con: 10,
+  int: 12,
+  wis: 11,
+  cha: 17,
+  alignment: ALIGNMENT.LN,
+  class: 'Farmer',
+  physical_description: 'foobar',
+  personality_description: 'foobar',
+  history: 'foobar',
+  plot_hook: 'foobar',
+},
+{
+  first_name: 'Sir Reginald',
+  last_name: 'Barnaby Rutherford',
+  portrait: undefined,
+  age: 76,
+  gender: true,
+  height_inches: 72,
+  weight_lbs: 160,
+  str: 18,
+  dex: 14,
+  con: 10,
+  int: 12,
+  wis: 11,
+  cha: 17,
+  alignment: ALIGNMENT.LN,
+  class: 'Knight',
+  physical_description: 'Old knight guy',
+  personality_description: 'HUUUUUUUURRRRRAHHHH',
+  history: 'Campaigns, crusades, wars, oh my',
+  plot_hook: 'foobar',
+}]
 
 export interface HomepageProps extends DefaultHomepageProps {
   npc?: NPC | NPC[]
 }
 
+let index = 0;
+
 function Homepage_(props: HomepageProps, ref: HTMLElementRefOf<"div">) {
   const rest = props as DefaultHomepageProps;
+  const [npcArray, setNpcArray] = useState<NPC[] | undefined>(undefined);
+  const [npc, setNpc] = useState<NPC | undefined>(undefined);
 
-  return <PlasmicHomepage root={{ ref }} {...rest} />;
+  function setPrevNpc(): void {
+    if (!npcArray) {
+      return;
+    }
+
+    index--;
+    if (index < 0) {
+      index = npcArray.length - 1;
+    }
+
+    setNpc(npcArray[index]);
+  }
+
+  function setNextNpc(): void {
+    if (!npcArray) {
+      return;
+    }
+
+    index++;
+    if (index >= npcArray.length) {
+      index = 0;
+    }
+
+    setNpc(npcArray[index]);
+  }
+
+  return <PlasmicHomepage
+    root={{ ref }}
+    left={{
+      props: {
+        style: { opacity: npcArray && npcArray.length > 1 ? 1 : 0 },
+        click: () => setPrevNpc()
+      }
+    }}
+    right={{
+      props: {
+        style: { opacity: npcArray && npcArray.length > 1 ? 1 : 0 },
+        click: () => setNextNpc()
+      }
+    }}
+    info={{ props: { npc: npc } }}
+    generateSelector={{
+      props: {
+        generateButtonClick: () => {
+          const dummyArray = dummyNPCs;
+          setNpcArray(dummyArray)
+          setNpc(dummyArray[0])
+        }
+      }
+    }}
+    {...rest}
+  />;
 }
 
 const Homepage = React.forwardRef(Homepage_);
